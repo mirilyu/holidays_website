a6dc05d3a1333d3b4d846a845038bfa090c31c22
d400800 <d400801@gmail.com>
2017-06-30 11:57:08 +0300

    adding inner page, carousel

--- js/main.js
+++ js/main.js
@@ -1,9 +1,58 @@
 $( document ).ready(function() {
+
+	/* banner slider */
     var arrowBtn = $(".arrows li");
-    console.log(arrowBtn);
     var slides = $(".slide");
 
     arrowBtn.on("click", function() {
     	slides.toggleClass("active");
     });
+
+    /* carousel */
+    var carouselArrow = $(".carousel-arrow");
+    var arrowLeft = $(".carousel-arrow-left");
+    var arrowRight = $(".carousel-arrow-right");
+    var carousel = $('.carousel ul');
+    var isMoving = false;
+
+    carouselArrow.on("click", function() {
+
+    	if(!isMoving) {
+	    	var currTrans = carousel.css('transform').split(/[()]/)[1] || carousel.css('-webkit-transform').split(/[()]/)[1] || carousel.css('-ms-transform').split(/[()]/)[1] || carousel.css('-moz-transform').split(/[()]/)[1];
+	    	var pos = parseInt(currTrans.split(',')[4]);
+	    	if( $(this).hasClass("carousel-arrow-left") ) {
+	    		var newPos = pos+240;
+	    	} else {
+	    		var newPos = pos-240;
+	    	}
+	    	
+	    	console.log(newPos);
+
+	    	if(newPos == 0) {
+	    		arrowRight.addClass("hidden");
+	    	} else if(newPos > 0 && newPos < 1200) {
+	    		arrowRight.removeClass("hidden");
+	    		arrowLeft.removeClass("hidden");
+	    	} else {
+	    		arrowLeft.addClass("hidden");
+	    	}
+
+	    	if (newPos > 1200 || newPos < 0) {
+	    		return;
+	    	} else {
+				isMoving = true;
+				carousel.animate({ textIndent: 0 }, {
+				    step: function() {
+				      $(this).css('-moz-transform','translateX('+newPos+'px)');
+				      $(this).css('-webkit-transform','translateX('+newPos+'px)');
+				      $(this).css('-ms-transform','translateX'+newPos+'px)');
+				      $(this).css('transform','translateX('+newPos+'px)');
+				    },
+				    duration: 300,
+				    complete: function(){ isMoving = false }
+				});
+	    	}
+    	}
+    })
+
 });
